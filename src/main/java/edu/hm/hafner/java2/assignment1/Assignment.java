d333c16fcfa76f2868f384c8de3c373b4c659b41
Jeremy Costantino <jeremy-costantino@gmx.de>
2023-03-30 14:09:39 +0200

    added solution for assignment1

--- src/main/java/edu/hm/hafner/java2/assignment1/Assignment.java
+++ src/main/java/edu/hm/hafner/java2/assignment1/Assignment.java
@@ -4,4 +4,59 @@ package edu.hm.hafner.java2.assignment1;
  * Representation of an assignment.
  */
 public class Assignment {
+    private final int number;
+    private final int numberTestCases;
+    private boolean[] testCases;
+
+    public Assignment(final int number, final int numberTestCases) {
+        this.number = number;
+        this.numberTestCases = numberTestCases;
+        this.testCases = new boolean[numberTestCases];
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getTests() {
+        return numberTestCases;
+    }
+
+    public int getGreen() {
+        int count = 0;
+        for (boolean test : testCases) {
+            if (test) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getRed() {
+        int count = 0;
+        for (boolean test : testCases) {
+            if (!test) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getPercentage() {
+        if (getRed() == 0) {
+            return 100;
+        }
+        float percentage = (float) getGreen() / getTests() * 100;
+        return (int) percentage;
+    }
+
+    public void solve(final int index) {
+        if (index >= 0 && index < testCases.length) {
+            testCases[index] = true;
+        }
+    }
+
+    public boolean isSufficient() {
+        return getPercentage() >= 50;
+    }
 }
--- src/main/java/edu/hm/hafner/java2/assignment1/Exam.java
+++ src/main/java/edu/hm/hafner/java2/assignment1/Exam.java
@@ -4,4 +4,55 @@ package edu.hm.hafner.java2.assignment1;
  * An exam contains several assignments.
  */
 public class Exam {
+    private Assignment[] assignments = new Assignment[]{};
+
+    public int getSize() {
+        return assignments.length;
+    }
+
+    public void addAssignment(final Assignment assignment) {
+        int currentLenght = assignments.length;
+        Assignment[] newArray = new Assignment[currentLenght + 1];
+        for (int i = 0; i < currentLenght; i++) {
+            newArray[i] = assignments[i];
+        }
+        newArray[currentLenght] = assignment;
+        assignments = newArray;
+    }
+
+    public Assignment getAssignment(final int index) {
+        return assignments[index];
+    }
+
+    public int getScore() {
+        if (assignments.length < 1) {
+            return 100;
+        }
+        int percentageSum = 0;
+        for (Assignment assignment: assignments) {
+            percentageSum += assignment.getPercentage();
+        }
+        return percentageSum / assignments.length;
+    }
+
+    public boolean isSufficient() {
+        return getScore() >= 50;
+    }
+
+    public int getGrade() {
+        if (getScore() >= 95) {
+            return 1;
+        } else if (getScore() >= 80) {
+            return 2;
+        } else if (getScore() >= 65) {
+            return 3;
+        } else if (getScore() >= 50) {
+            return 4;
+        } else if (getScore() > 0) {
+            return 5;
+        } else {
+            return 6;
+        }
+    }
+
 }
